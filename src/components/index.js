9a11aad61ff5d9e42021f1437f81e09a8e72a8a2
AmirHosein Deldar <Amirhoseindeldar@yahoo.com>
2024-04-03 08:46:44 +0330

    finall coommiit

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,5 @@
-import SubHeading from './SubHeading/SubHeading';
-import Newsletter from './Footer/Newsletter';
-import FooterOverlay from './Footer/FooterOverlay';
-import MenuItem from './Menuitem/MenuItem';
-import Navbar from './Navbar/Navbar';
+import SubHeading from "./SubHeading/SubHeading";
+import MenuItem from "./Menuitem/MenuItem";
+import Navbar from "./Navbar/Navbar";
 
-export {
-  SubHeading,
-  Newsletter,
-  FooterOverlay,
-  MenuItem,
-  Navbar,
-};
+export { SubHeading, MenuItem, Navbar };
